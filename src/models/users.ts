2573949e55bf493c082354c1689d0701240ee528
hugogaytan10 <estardus9@gmail.com>
2022-07-31 10:23:22 -0500

    Heroku

--- src/models/users.ts
+++ src/models/users.ts
@@ -58,7 +58,7 @@ class User extends Database {
             connection.query(`SELECT * FROM usuarios where correo = ? and contrasenia = ?`,
             [user.correo, hash], (error, results, fields)=>{
                 if(error) reject(error);
-                if (results[0] !== undefined) {
+                if (results !== undefined) {
                     const accessToken = this.generateAccessToken(results[0].id);
                     const userFind = {
                         usuario: results[0],
