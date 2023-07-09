cb86fa20e519356864d162679917b4dc95d2646e
hugogaytan10 <estardus9@gmail.com>
2023-07-09 12:42:48 -0600

    Consulta de estadisticas agregada

--- src/models/statistics.ts
+++ src/models/statistics.ts
@@ -1,5 +1,4 @@
 import Database from "../services/database";
-
 class Statistic extends Database {
     constructor(){
         super({table: 'estadisticas'});
--- src/services/mysql.ts
+++ src/services/mysql.ts
@@ -1,11 +1,11 @@
 const mysql = require('mysql');
 const mysqlConfig = mysql.createConnection({
-    host: 'ravekh.cd6upsy0rosm.us-east-1.rds.amazonaws.com',
-    //host : 'localhost',
-    user: 'admin',
-    //user     : 'root',
-    password: 'ravekh123',
-    //password : 'root',
+    //host: 'ravekh.cd6upsy0rosm.us-east-1.rds.amazonaws.com',
+    host : 'localhost',
+    //user: 'admin',
+    user     : 'root',
+    //password: 'ravekh123',
+    password : 'root',
     database: 'rebk_db'
 });
 export default mysqlConfig;
